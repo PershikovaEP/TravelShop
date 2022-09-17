4044254c61a34ab06522b6acf53da441ea5e77a1
Elena Pershikova <zem524@mail.ru>
2022-09-18 00:28:10 +0300

    update plan.md and create a test automation project

---
+++ src/test/java/ru/netology/test/ShopTest.java
@@ -0,0 +1,28 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import io.qameta.allure.selenide.AllureSelenide;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class ShopTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:8080");
+    }
+
+
+}
